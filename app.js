89f12d79b74c2492be4a92136e37102073fc4bda
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2023-08-04 02:20:56 +0600

    Add files via upload
    next 2

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-a = [6, 4, 5, 4, 3, 2,8,3,2,5];
+a = [4, 2, 9, 3, 1, 8,5,7,0,10,5,3,9];
 
 
-  
-
-for(var i=0; i<10; i++){
-    for(var j=i+1; j<10; j++){
+for(var i=0; i<a.length-1; i++){
+    for(var j=i+1; j<a.length; j++){
         if(a[i]>a[j]){
             a[i]=a[i]+a[j];
             a[j]=a[i]-a[j];
@@ -16,30 +14,27 @@ for(var i=0; i<10; i++){
 
 
 
-function nowArr(ar){
-    
+function nowArr(ar) {
+    class Box {
+        constructor(value) {
+            this.container = document.querySelector('.container')
+            this.value = value
+        }
 
-class Box{
-    constructor(value){
-        this.container = document.querySelector('.container')
-        this.value = value
+
+        draw() {
+            this.div = document.createElement('div')
+            this.div.className = 'box'
+            this.div.innerText = this.value
+            this.div.style.height = `${this.value * 50}px`
+            //console.log(this.div.style.height)
+            this.container.append(this.div)
+        }
     }
-    
 
-    draw(){
-        this.div = document.createElement('div')
-        this.div.className = 'box'
-        this.div.innerText = this.value
-        this.div.style.height = `${this.value*50}px`
-        console.log(this.div.style.height)
-        this.container.append(this.div)
+    for (const i of ar) {
+
+        b = new Box(i)
+        b.draw()
     }
-}
-
-for(const i of ar){
-
-    b = new Box(i)
-    b.draw()
-    
-}
 } 
